d768c2414a8429021223ff413cb10ff3c2f66e75
Shreya Pandey <shreyapsp03@gmail.com>
2025-08-16 07:06:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Building2, ListChecks, LogOut, Loader2, Route, LayoutDashboard, ScanEye, Send, Wrench } from 'lucide-react';
 import { signOutUser } from '@/lib/firebase/service';
 import Image from 'next/image';
+import { Badge } from '@/components/ui/badge';
 
 export default function Home() {
   const router = useRouter();
