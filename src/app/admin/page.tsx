69477362454ed6036e8d0493d12983d745074445
Shreya Pandey <shreyapsp03@gmail.com>
2025-10-02 12:02:43 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -32,6 +32,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
